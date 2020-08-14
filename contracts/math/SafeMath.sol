1ab85f2f52ffebef8b209f30b74013ca7d309e87
raullenchai <raullenchai@gmail.com>
2020-08-13 21:46:07 -0700

    import libs

---
+++ contracts/math/SafeMath.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.4.21;
+
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+
+  /*@CTK SafeMath_mul
+    @tag spec
+    @post __reverted == __has_assertion_failure
+    @post __has_assertion_failure == __has_overflow
+    @post __reverted == false -> c == a * b
+    @post msg == msg__post
+   */
+  /* CertiK Smart Labelling, for more details visit: https://certik.org */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
+    }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  /*@CTK SafeMath_div
+    @tag spec
+    @pre b != 0
+    @post __reverted == __has_assertion_failure
+    @post __has_overflow == true -> __has_assertion_failure == true
+    @post __reverted == false -> __return == a / b
+    @post msg == msg__post
+   */
+  /* CertiK Smart Labelling, for more details visit: https://certik.org */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  /*@CTK SafeMath_sub
+    @tag spec
+    @post __reverted == __has_assertion_failure
+    @post __has_overflow == true -> __has_assertion_failure == true
+    @post __reverted == false -> __return == a - b
+    @post msg == msg__post
+   */
+  /* CertiK Smart Labelling, for more details visit: https://certik.org */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  /*@CTK SafeMath_add
+    @tag spec
+    @post __reverted == __has_assertion_failure
+    @post __has_assertion_failure == __has_overflow
+    @post __reverted == false -> c == a + b
+    @post msg == msg__post
+   */
+  /* CertiK Smart Labelling, for more details visit: https://certik.org */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
