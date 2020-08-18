f0ffe690113a72f8ca34a353989ce52bf62e22c5
zhi <thecoderzhi@gmail.com>
2020-08-18 14:50:12 -0700

    add unit tests for MinterPool

---
+++ test/MinterPool.test.js
@@ -0,0 +1,23 @@
+const MinterPool = artifacts.require('MinterPool');
+const ShadowToken = artifacts.require('ShadowToken');
+const {assertAsyncThrows} = require('./assert-async-throws');
+
+contract('MinterPool', function([owner, stranger, fakeTokenAddress]) {
+    beforeEach(async function() {
+        this.minterPool = await MinterPool.new();
+        this.shadowToken = await ShadowToken.new(this.minterPool.address, fakeTokenAddress);
+    });
+    it('is owner', async function() {
+        assert.equal(await this.shadowToken.minter(), this.minterPool.address);
+    });
+    it('is owner', async function() {
+        assert.equal(await this.shadowToken.balanceOf(owner), 0);
+        await this.minterPool.mint(this.shadowToken.address, owner, 10000000000);
+        assert.equal(await this.shadowToken.balanceOf(owner), 10000000000);
+    });
+    it('not owner', async function() {
+        assert.equal(await this.shadowToken.balanceOf(stranger), 0);
+        await assertAsyncThrows(this.minterPool.mint(this.shadowToken.address, stranger, 1234567, {from: stranger}));
+        assert.equal(await this.shadowToken.balanceOf(stranger), 0);
+    });
+});
