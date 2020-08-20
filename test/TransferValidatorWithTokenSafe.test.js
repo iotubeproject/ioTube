1cd252092dd002fdfcfd3934b58238537737e611
zhi <thecoderzhi@gmail.com>
2020-08-19 18:30:07 -0700

    add unit test for Transfer Validator With Token Safe

--- test/TransferValidatorWithTokenSafe.test.js
+++ test/TransferValidatorWithTokenSafe.test.js
@@ -1,19 +1,20 @@
-const MinterPool = artifacts.require('MinterPool');
-const TransferValidatorWithMinterPool = artifacts.require('TransferValidatorWithMinterPool');
+const TokenSafe = artifacts.require('TokenSafe');
+const TransferValidatorWithTokenSafe = artifacts.require('TransferValidatorWithTokenSafe');
 const TokenList = artifacts.require('TokenList');
 const ShadowToken = artifacts.require('ShadowToken');
 const VoterList = artifacts.require('VoterList');
 const {assertAsyncThrows} = require('./assert-async-throws');
 
-contract('TransferValidatorWithMinterPool', function([owner, minter, sender, receiver, voter1, voter2, voter3, voter4, fakeTokenAddress]) {
+contract('TransferValidatorWithTokenSafe', function([owner, minter, sender, receiver, voter1, voter2, voter3, voter4, fakeTokenAddress]) {
     beforeEach(async function() {
-        this.minterPool = await MinterPool.new();
-        // use shadow token as mintable erc20 token
-        this.shadowToken = await ShadowToken.new(this.minterPool.address, fakeTokenAddress);
+        this.tokenSafe = await TokenSafe.new();
+        // use shadow token as standard erc20 token
+        this.shadowToken = await ShadowToken.new(minter, fakeTokenAddress);
+        await this.shadowToken.mint(this.tokenSafe.address, 100000000, {from: minter});
         this.tokenList = await TokenList.new();
         this.voterList = await VoterList.new();
-        this.validator = await TransferValidatorWithMinterPool.new(10, this.minterPool.address, this.tokenList.address, this.voterList.address);
-        await this.minterPool.transferOwnership(this.validator.address);
+        this.validator = await TransferValidatorWithTokenSafe.new(10, this.tokenSafe.address, this.tokenList.address, this.voterList.address);
+        await this.tokenSafe.transferOwnership(this.validator.address);
     });
     it('voter not in list', async function() {
         await assertAsyncThrows(this.validator.vote(this.shadowToken.address, 0, sender, receiver, 12345, {from: voter1}));
@@ -30,6 +31,7 @@ contract('TransferValidatorWithMinterPool', function([owner, minter, sender, rec
         await this.validator.vote(this.shadowToken.address, 0, sender, receiver, 12345, {from: voter1});
         assert.equal(await this.validator.settled(key), true);
         assert.equal(await this.shadowToken.balanceOf(receiver), 12345);
+        assert.equal(await this.shadowToken.balanceOf(this.tokenSafe.address), 99987655);
     });
     it('two voters', async function() {
         await this.voterList.addVoter(voter1);
@@ -42,6 +44,7 @@ contract('TransferValidatorWithMinterPool', function([owner, minter, sender, rec
         await this.validator.vote(this.shadowToken.address, 0, sender, receiver, 12345, {from: voter2});
         assert.equal(await this.validator.settled(key), true);
         assert.equal(await this.shadowToken.balanceOf(receiver), 12345);
+        assert.equal(await this.shadowToken.balanceOf(this.tokenSafe.address), 99987655);
     });
     it('three voters', async function() {
         await this.voterList.addVoter(voter1);
@@ -57,6 +60,7 @@ contract('TransferValidatorWithMinterPool', function([owner, minter, sender, rec
         await this.validator.vote(this.shadowToken.address, 0, sender, receiver, 12345, {from: voter3});
         assert.equal(await this.validator.settled(key), true);
         assert.equal(await this.shadowToken.balanceOf(receiver), 12345);
+        assert.equal(await this.shadowToken.balanceOf(this.tokenSafe.address), 99987655);
     });
     it('four voters', async function() {
         await this.voterList.addVoter(voter1);
@@ -72,13 +76,15 @@ contract('TransferValidatorWithMinterPool', function([owner, minter, sender, rec
         assert.equal(await this.validator.settled(key), false);
         await this.validator.vote(this.shadowToken.address, 0, sender, receiver, 12345, {from: voter3});
         assert.equal(await this.validator.settled(key), true);
+        await this.validator.vote(this.shadowToken.address, 0, sender, receiver, 12345, {from: voter4});
         assert.equal(await this.shadowToken.balanceOf(receiver), 12345);
+        assert.equal(await this.shadowToken.balanceOf(this.tokenSafe.address), 99987655);
     });
     it('upgrade', async function() {
-        assert.equal(await this.minterPool.owner(), this.validator.address);
+        assert.equal(await this.tokenSafe.owner(), this.validator.address);
         await assertAsyncThrows(this.validator.upgrade(owner, {from: sender}));
-        assert.equal(await this.minterPool.owner(), this.validator.address);
+        assert.equal(await this.tokenSafe.owner(), this.validator.address);
         await this.validator.upgrade(owner);
-        assert.equal(await this.minterPool.owner(), owner);
+        assert.equal(await this.tokenSafe.owner(), owner);
     });
 });
