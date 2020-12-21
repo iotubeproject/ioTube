1a7db1ad1845a07fc852456e3f3ca8c865671f6b
zhi <thecoderzhi@gmail.com>
2020-12-20 19:16:02 -0700

    safe and cashier for non standard token

---
+++ contracts/iotube/NonStandardTokenSafe.sol
@@ -0,0 +1,11 @@
+pragma solidity <6.0 >=0.4.24;
+
+import "../ownership/Ownable.sol";
+
+contract NonStandardTokenSafe is Ownable {
+    function withdrawToken(address _token, address _to, uint256 _amount) public onlyOwner returns (bool) {
+        // selector = bytes4(keccak256(bytes('transfer(address,uint256)')))
+        (bool success, bytes memory data) = _token.call(abi.encodeWithSelector(0xa9059cbb, _to, _amount));
+        return success && (data.length == 0 || abi.decode(data, (bool)));	    
+    }
+}
