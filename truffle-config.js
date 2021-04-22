4b54d3ed0a220525a015d5186363ed080687f90b
ququzone <xueping.yang@gmail.com>
2021-04-22 22:36:13 +0800

    uncomment hdwallet

--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,7 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
