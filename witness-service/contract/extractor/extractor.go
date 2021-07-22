1f2511620d546dabd3715b72f7cb73b1d90561d2
zhi <thecoderzhi@gmail.com>
2021-07-22 16:13:16 -0700

    update bin and contract go files

--- witness-service/contract/extractor/extractor.go
+++ witness-service/contract/extractor/extractor.go
@@ -31,12 +31,16 @@ func main() {
 
 	var jsonContent map[string]interface{}
 	json.Unmarshal([]byte(byteValue), &jsonContent)
-	if content != "abi" && content != "bin" {
+	switch content {
+	case "abi":
+		c, err := json.Marshal(jsonContent[content])
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(string(c))
+	case "bytecode":
+		fmt.Println(jsonContent[content].(string)[2:])
+	default:
 		log.Fatalln("unknown content", content)
 	}
-	c, err := json.Marshal(jsonContent[content])
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(string(c))
 }
