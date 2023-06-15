b386de31766e95c2eb81d37a297b91ec41d0fb26
ququzone <xueping.yang@gmail.com>
2023-06-15 15:41:16 +0800

    fix load private key error

--- hardhat.config.js
+++ hardhat.config.js
@@ -43,27 +43,22 @@ module.exports = {
     },
     mainnet: {
       url: process.env.MAINNET_RPC_URL,
-      accounts: accounts,
       chainId: 1,
     },
     iotex: {
         url: 'https://babel-api.mainnet.iotex.io',
-        accounts: accounts,
         chainId: 4689,
     },
     iotex_test: {
         url: 'https://babel-api.testnet.iotex.io',
-        accounts: accounts,
         chainId: 4690,
     },
     bsc: {
       url: 'https://bsc-dataseed.binance.org',
-      accounts: accounts,
       chainId: 56,
     },
     polygon: {
         url: 'https://polygon-rpc.com/',
-        accounts: accounts,
         chainId: 137,
     },
   }
