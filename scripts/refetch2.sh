88989c46a67f945e49c7b7f7a2b7c1010937563d
zhi <thecoderzhi@gmail.com>
2023-12-05 00:37:50 +0800

    fix refetch script

--- scripts/refetch2.sh
+++ scripts/refetch2.sh
@@ -5,6 +5,7 @@ chainmap["iotex"]="9081"
 chainmap["ethereum"]="9083"
 chainmap["matic"]="9085"
 chainmap["bsc"]="9087"
+port=${chainmap[$chain]}
 echo "refetch from port:${port}"
-curl -X POST localhost:$port/fetch -d '{"heights": '$@'}'
+curl -X POST http://localhost:$port/fetch -d '{"heights": "'$@'"}'
 echo "...done"
