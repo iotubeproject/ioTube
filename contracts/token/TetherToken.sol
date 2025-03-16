9f90f6b8551d0e95358b0ba5241556ceb6cc1106
jimmycathy <clonecode@outlook.com>
2025-03-16 19:01:52 +0800

    chore: remove redundant words in comment
    Signed-off-by: jimmycathy <clonecode@outlook.com>

--- contracts/token/TetherToken.sol
+++ contracts/token/TetherToken.sol
@@ -75,7 +75,7 @@ contract NSBasicToken is Ownable, NSERC20Basic {
 
     /**
     * @dev Gets the balance of the specified address.
-    * @param _owner The address to query the the balance of.
+    * @param _owner The address to query the balance of.
     * @return An uint representing the amount owned by the passed address.
     */
     function balanceOf(address _owner) public view returns (uint balance) {
