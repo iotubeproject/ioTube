e38abe5b4612351399aeb3b695098f4afb1939ec
Liuhaai <haixiang@iotex.io>
2025-06-30 12:42:09 -0700

    fix bugs in WrapperCashierRouter contract

--- contracts/iotube/WrapperCashierRouter.sol
+++ contracts/iotube/WrapperCashierRouter.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-interface IWrapper {
-    function target_token() external view returns (IERC20Burnable);
+interface ISwapper {
+    function target_token() external view returns (IERC20);
     function source_token() external view returns (IERC20);
     function deposit(uint256 _amount) external;
     function withdraw(uint256 _amount) external;
@@ -11,40 +11,40 @@ interface IWrapper {
 
 interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
-    function transfer(address to, uint256 amount) external returns (bool);
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
 }
 
 interface ICashier {
-    function depositTo(address _token, address _to, uint256 _amount) external payable;
+    function depositTo(address _token, address _to, uint256 _amount, bytes memory _payload) external payable;
 }
 
-interface IERC20Burnable is IERC20 {
-    function mint(address to, uint256 amount) external returns (bool);
-    function burn(uint256 amount) external;
-}
-
-contract WrapperCashierRouter {
-    IWrapper public wrapper;
-    IERC20Burnable public target_token;
+contract SwapperCashierRouter {
+    ISwapper public swapper;
+    address public target_token;
     address public source_token;
-    constructor(IWrapper _wrapper) {
-        wrapper = _wrapper;
-        target_token = _wrapper.target_token();
-        source_token = address(_wrapper.source_token());
-        approveWrapper();
+    constructor(ISwapper _swapper) {
+        swapper = _swapper;
+        target_token = address(_swapper.target_token());
+        source_token = address(_swapper.source_token());
+        _approve();
     }
 
-    function approveWrapper() public {
-        require(safeApprove(address(target_token), address(wrapper), type(uint256).max), "failed to approve allowance to wrapper");
+    function _approve() private {
+        require(safeApprove(target_token, address(swapper), type(uint256).max), "failed to approve allowance to swapper");
     }
 
-    function depositTo(address _cashier, address _to, uint256 _amount) public payable {
-        require(wrapper.balance() >= _amount, "insufficient balance");
-        require(target_token.transferFrom(msg.sender, address(this), _amount), "failed to transfer target token");
-        wrapper.withdraw(_amount);
+    function depositTo(address _cashier, address _to, uint256 _amount, bytes memory _payload) public payable {
+        require(swapper.balance() >= _amount, "swapper insufficient balance");
+        require(safeTransferFrom(target_token, msg.sender, address(this), _amount), "failed to transfer target token");
+        swapper.withdraw(_amount);
         require(safeApprove(source_token, _cashier, _amount), "failed to approve allowance to cashier");
-        ICashier(_cashier).depositTo{value: msg.value}(source_token, _to, _amount);
+        ICashier(_cashier).depositTo{value: msg.value}(source_token, _to, _amount, _payload);
+    }
+
+    function safeTransferFrom(address _token, address _from, address _to, uint256 _amount) internal returns (bool) {
+        // selector = bytes4(keccak256(bytes('transferFrom(address,address,uint256)')))
+        (bool success, bytes memory data) = _token.call(abi.encodeWithSelector(0x23b872dd, _from, _to, _amount));
+        return success && (data.length == 0 || abi.decode(data, (bool)));
     }
 
     function safeApprove(address _token, address _spender, uint256 _amount) internal returns (bool) {
